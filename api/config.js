952158e6bbf5b6ca461f4587ce683bf5d5412b60
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-12-03 22:30:28 -0700

    added some api

---
+++ api/config.js
@@ -0,0 +1,9 @@
+module. exports = {
+    APP_ID: process.env.APP_ID,
+    API_KEY: process.env,API_KEY, 
+    BASE_URL: `https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972`,
+}; 
+
+
+
+
