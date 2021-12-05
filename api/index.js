f055f2de971026e3e373d5e1196b9c16a18b0b5d
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-12-04 22:22:11 -0700

    worked on food app

--- api/index.js
+++ api/index.js
@@ -3,3 +3,29 @@ const url = require('url');
 const axios = require ('axios'); 
 const chalk = require ('chalk'); 
 const config = require ('./config');
+
+const headers = {
+    'Content-Type': 'application/json', 
+    'Access-Control-Allow-Origin': '*', 
+    'Access-Control-Allow-Methods': 'GET'
+};
+
+
+const server = createServer((req, res) =>{
+    const requestURL = url.parse(req.url)
+})
+
+//ミックスで検索できるように
+
+const decodeParams = searchParamas => Array 
+    .from(searchParamas.keys())
+    .reduce((acc, key) =>
+    ({...acc, [key]: searchParamas.get(key)}. {}); 
+
+const server = createServer ((req, res) => {
+    const requestURL = url.parse(req.url); 
+    const decodedParams = decodeParams = decodeParams (new URLSearchParams(requestURL.serach)); 
+    const { search, location, city = 'Orem', state = 'UT'} = decodedParams; 
+
+    const targetURL = `${config.BASE_URL}/${state.toUpperCase()}/${config.BASE}`
+})
