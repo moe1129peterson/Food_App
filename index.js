382b82c0096b2c8ef537c444ba138e27060a81a9
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-12-12 19:54:49 -0700

    adjusted paths for connecting backend to frontend

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const connection = require('./config/db.config')
 connection.once('open', () => console.log('DB Connected'))
 connection.on('error', () => console.log('Error'))
 
+// Serve Static Folder
+app.use(express.static('./public'))
+
 // Routes Config
 app.use(express.json({
     extended: false
 })) //parse incoming request body in JSON format.
-app.use('/', require('./routes/redirect'))
+app.use('/api', require('./routes/redirect'))
 app.use('/api/url', require('./routes/url'))
 
 //Listen for incoming requests
