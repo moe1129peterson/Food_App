5ab539ac6b4b1d5968c4fbc723416aad9f96392a
moe1129peterson <moe1129tsuzuki@gmail.com>
2021-12-14 21:26:28 -0700

    adjusted readme and styling

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DGM3760_Final
-Final Project for DGM 3760
+URsmalL a URL shortener app that was created using vanilla JavaScript on the frontend, and uses MongoDB for its database. The app uses its own API and is being hosted on Heroku. This is a Final Project for DGM 3760.
+
+# Endpoints
+-Create:
+url.js line 15
+
+-Read: 
+redirect.js line 11
+
+-Update:
+redirect.js line 11
+
+-Delete:
+url.js line 61
+
--- public/index.js
+++ public/index.js
@@ -3,7 +3,6 @@ const shortUrl = document.getElementById('shortUrl')
 let shortId
 
 
-
 let submitUrl = async () => {
     let response = await axios({
         method: 'POST', 
@@ -16,10 +15,17 @@ let submitUrl = async () => {
         shortUrl.innerHTML = response.data.shortUrl
         shortUrl.style.display = 'block'
         deleteBtn.style.display = 'block'
+        copyButton.style.display = 'block'
         shortId = response.data._id
     }
 }
 
+const copyButton = document.getElementById('copyBtn');
+
+copyButton.addEventListener('click', (event) => {
+    const content = document.getElementById('shortUrl').textContent;
+    navigator.clipboard.writeText(content);
+});
 
 const subBtn = document.getElementById('shortenURLbtn')
 subBtn.addEventListener('click', submitUrl)
@@ -35,10 +41,12 @@ const deleteUrl = async () => {
         shortUrl.innerHTML = response.data.shortUrl
         shortUrl.style.display = 'none'
         deleteBtn.style.display = 'none'
+        copyButton.style.display = 'none'
     }
 }
 
 
-
 const deleteBtn = document.getElementById('deleteBtn')
 deleteBtn.addEventListener('click', deleteUrl)
+
+
